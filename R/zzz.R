ab3ff1726304f6f3849c3b96c8bd4cff6810ba6c
djwtouw <75936485+djwtouw@users.noreply.github.com>
2023-07-11 14:27:45 +0200

    Added .onAttach()

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,16 @@
-.onUnload <- function ( libpath ) {
+.onUnload <- function (libpath)
+{
     library.dynam.unload("CCMMR", libpath)
+}
+
+
+.onAttach <- function(libname, pkgname)
+{
+    package_citation = paste("\nTouw, D.J.W., Groenen, P.J.F., and Terada, Y.",
+                             "(2022). Convex Clustering through MM: An",
+                             "Efficient Algorithm to Perform Hierarchical",
+                             "Clustering. arXiv preprint arXiv:2211.01877.\n")
+    message("Thank you for using CCMMR!")
+    message("To acknowledge our work, please cite the paper:")
+    message(package_citation)
 }
